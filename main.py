2669b4630219213f87a1a478d5850111ec07a08f
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-09-12 20:46:31 +0200

    Adding main game loop first try

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ pygame.init()
 #Initial set up
 WIDTH = 400
 HEIGHT = 500
-screen = pygame.display.set_mode(WIDTH, HEIGHT)
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('2048')
 
-font = pygame.font.Font('freesanbold.ttf', 24)
+font = pygame.font.Font('freesansbold.ttf', 24)
 
 timer = pygame.time.Clock()
 fps = 60
+
+#Main game loop
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill("gray")
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            
+    pygame.display.flip()
+pygame.quit()

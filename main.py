5309d2f2c8361cf4a5582244df5233314e14ce02
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-09-12 16:10:25 +0200

    initial set up ready

--- main.py
+++ main.py
@@ -2,4 +2,13 @@ import pygame
 import random
 pygame.init()
 
+#Initial set up
+WIDTH = 400
+HEIGHT = 500
+screen = pygame.display.set_mode(WIDTH, HEIGHT)
+pygame.display.set_caption('2048')
 
+font = pygame.font.Font('freesanbold.ttf', 24)
+
+timer = pygame.time.Clock()
+fps = 60

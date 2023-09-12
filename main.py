3b919770c732b45d160a1f4cc55e2ae692d902db
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-09-12 20:56:39 +0200

    drawing the background to the board

--- main.py
+++ main.py
@@ -13,11 +13,28 @@ font = pygame.font.Font('freesansbold.ttf', 24)
 timer = pygame.time.Clock()
 fps = 60
 
+#Drawing the background for the board
+def draw_board():
+    pygame.draw.rect(screen,(200, 200, 200) [0 ,0, 400, 400], 0, 10)
+    pass
+
+#Drawing the numbers(objects)
+def draw_pieces():
+    pass
+
+#Adding new numbers randomly
+def spawn_pieces():
+    pass
+
 #Main game loop
 run = True
 while run:
     timer.tick(fps)
     screen.fill("gray")
+    
+    draw_board()
+    draw_pieces()
+    spawn_pieces()
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
